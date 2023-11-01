685cfaaa150e4f91ad85465868769a7a1fdcbe4a
shapanmiah <shapanmiah19@gmail.com>
2023-11-01 23:35:29 +0600

    update

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,9 @@ import styles from './page.module.css'
 export default function Home() {
   return (
     <main className={styles.main}>
-      home
+      <div className={styles.container}>
+        page
+      </div>
     </main>
   )
 }
