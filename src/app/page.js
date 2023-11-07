aaafd14deeb246f80affe1ea6bec15c8371ef87f
shapanmiah <shapanmiah19@gmail.com>
2023-11-07 21:14:04 +0600

    update

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,12 @@
 import Image from 'next/image'
 import styles from './page.module.css'
+import Link from 'next/link'
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.container}>
-        page
+        <Link href="/about">about</Link>
       </div>
     </main>
   )
